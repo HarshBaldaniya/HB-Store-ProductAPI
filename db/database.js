4efa519d1d3b314797c536ca8b81fe5e0a18233f
Harsh Baldaniya <baldaniyaharsh5@gmail.com>
2023-07-17 23:23:42 +0530

    final

--- db/database.js
+++ db/database.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDb = (uri) => {
-  mongoose.connect(uri, {
+  console.log("Connect to Database")
+  return mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: "products",
   });
-  const db = mongoose.connection;
-  db.on("error", (err) => console.error(err));
-  db.once("open", () => console.log("Connect to Database"));
 };
 
 module.exports = connectDb;
